be888d73cb489e20d6d388e660aed1ec64ab146e
Marcos <mjblufstein@dc.uba.ar>
2019-03-24 14:47:02 -0300

    implementacion backtracking. falta optimalidad

--- src/backtracking.cpp
+++ src/backtracking.cpp
@@ -12,15 +12,89 @@ using vi = vector<int>;
 using vll = vector<ll>;
 
 
-int main() {
-	
-	int n;
-	cin >> n; 					//cantidad de pedidos disponibles
+vector<pll> pedidos;
+ll w;
 
-	int w;
+
+//SIN PODAS
+void solveBacktracking(pll& res, pll& parcial, int indice)
+{
+	for(int i = indice; i < pedidos.size(); i++)
+	{
+		//incluyo pedidos[i] en la solucion
+		parcial.first += pedidos[i].first;
+		parcial.second += pedidos[i].second;
+
+		if(parcial.first <= w && parcial.second > res.second || parcial.second == res.second && parcial.first < res.first)
+		{
+			res.first = parcial.first;
+			res.second = parcial.second;
+		}
+
+		//pasamos al proximo elemento
+		solveBacktracking(res, parcial, i+1);
+
+		//excluimos pedidos[i] de la solucion
+		parcial.first -= pedidos[i].first;
+		parcial.second -= pedidos[i].second;
+	}
+
+	return;
+}
+
+
+//CON PODAS
+void solveBacktrackingPodas(pll& res, pll& parcial, int indice)
+{
+	for(int i = indice; i < pedidos.size(); i++)
+	{
+		//incluyo pedidos[i] en la solucion
+		parcial.first += pedidos[i].first;
+		parcial.second += pedidos[i].second;
+
+
+		if(parcial.first <= w && parcial.second > res.second || parcial.second == res.second && parcial.first < res.first)
+		{
+			res.first = parcial.first;
+			res.second = parcial.second;
+		}
+
+
+		//pasamos al proximo elemento
+		//poda por factibilidad. Solo llamamos si el peso parcial es menor al limite
+		if(parcial.first < w) solveBacktrackingPodas(res, parcial, i+1);
+
+		//excluimos pedidos[i] de la solucion
+		parcial.first -= pedidos[i].first;
+		parcial.second -= pedidos[i].second;
+	}
+
+	return;
+}
+
+
+
+
+int main() {
+
+	ll n;
+	cin >> n; 				//cantidad de pedidos disponibles
+
 	cin >> w;					//capacidad del comprador
 
+	for(int i = 0; i < n; i++)
+	{
+		ll wi, pi;
+		cin>>wi>>pi;
+		pedidos.push_back(make_pair(wi, pi));
+	}
 
-	
+
+	pll inicial = make_pair(0,0);
+	pll res = inicial;
+	solveBacktrackingPodas(res, inicial, 0);
+
+	cout<<res.second<<'\n';
+
 	return 0;
 }
