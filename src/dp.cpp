6fc3a66f0d55ee92168dd639a0831e15ad032519
mmaximiliano <maxii.martino@gmail.com>
2019-03-28 01:52:41 -0300

    no funciona

--- src/dp.cpp
+++ src/dp.cpp
@@ -15,12 +15,12 @@ using vll = vector<ll>;
 
 int main() {
 	
-	ll n, w;							// w = capacidad del comprador
+	int n, w;							// w = capacidad del comprador
 	cin >> n >> w; 						// n = cantidad de pedidos disponibles
 
-	vector<pll> pedidos(n);				// vector con los valores wi, pi
+	vector<pii> pedidos(n);				// vector con los valores wi, pi
 	
-	ll wi, pi, maxPi;	
+	int wi, pi;	
 
 	for(int i = 0; i < n; i++)			// obtengo los valores wi, pi
 	{	
@@ -28,6 +28,25 @@ int main() {
 		pedidos[i] = make_pair(wi, pi);
 	}
 
-	
+	int dp[n][w];
+
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < w; j++){
+			if(i==0 || j==0){
+				dp[i][j] = 0;
+			}else{
+				if(pedidos[i-1].first <= j){
+					dp[i][j] = max(dp[i-1][j], pedidos[i-1].second + dp[i-1][j-pedidos[i].first]); 
+					
+				}else{
+					dp[i][j] = dp[i-1][j];
+				}
+
+			}
+		}
+	}
+
+
+	cout << dp[n][w] << "\n"; 
 	return 0;
 }
