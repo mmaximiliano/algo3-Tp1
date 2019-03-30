ee5a876018ce4c4e44ca391f6e03974309904f85
Marcos <mjblufstein@dc.uba.ar>
2019-03-30 17:42:55 -0300

    anda dp

--- src/dp.cpp
+++ src/dp.cpp
@@ -14,30 +14,30 @@ using vll = vector<ll>;
 
 
 int main() {
-	
+
 	int n, w;							// w = capacidad del comprador
 	cin >> n >> w; 						// n = cantidad de pedidos disponibles
 
 	vector<pii> pedidos(n);				// vector con los valores wi, pi
-	
-	int wi, pi;	
+
+	int wi, pi;
 
 	for(int i = 0; i < n; i++)			// obtengo los valores wi, pi
-	{	
+	{
 		cin>>wi>>pi;
 		pedidos[i] = make_pair(wi, pi);
 	}
 
-	int dp[n][w];
+	int dp[n+1][w+1];
 
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < w; j++){
+	for(int i = 0; i <= n; i++){
+		for(int j = 0; j <= w; j++){
 			if(i==0 || j==0){
 				dp[i][j] = 0;
 			}else{
 				if(pedidos[i-1].first <= j){
-					dp[i][j] = max(dp[i-1][j], pedidos[i-1].second + dp[i-1][j-pedidos[i].first]); 
-					
+					dp[i][j] = max(dp[i-1][j], pedidos[i-1].second + dp[i-1][j-pedidos[i-1].first]);
+
 				}else{
 					dp[i][j] = dp[i-1][j];
 				}
@@ -47,6 +47,6 @@ int main() {
 	}
 
 
-	cout << dp[n][w] << "\n"; 
+	cout << dp[n][w] << "\n";
 	return 0;
 }
