7cdcb0c81126eedb4db65ed57083ef750dbb879c
mmaximiliano <31483803+mmaximiliano@users.noreply.github.com>
2019-04-04 12:38:07 -0300

    comienzo informe

--- src/fuerzaBruta.cpp
+++ src/fuerzaBruta.cpp
@@ -20,22 +20,22 @@ bool compPi(pll &a, pll &b) 				// funcion de comparacion para max_element
 
 int main() {
 	
-	ll n, w;							// w = capacidad del comprador
-	cin >> n >> w; 						// n = cantidad de pedidos disponibles
+	ll n, w;								// w = capacidad del comprador
+	cin >> n >> w; 							// n = cantidad de pedidos disponibles
 
-	vector<pll> pedidos(n);				// vector con los valores wi, pi
+	vector<pll> pedidos(n);					// vector con los valores wi, pi
 	
 	ll wi, pi, maxPi;	
 
-	for(int i = 0; i < n; i++)			// obtengo los valores wi, pi
+	for(int i = 0; i < n; i++)				// obtengo los valores wi, pi
 	{	
 		cin>>wi>>pi;
 		pedidos[i] = make_pair(wi, pi);
 	}
 
-	vector<pll> subSets(1<<n);	// todos los subconjuntos
+	vector<pll> subSets(1<<n);				// todos los subconjuntos
 
-	for(int i = 0; i < (1<<n); i++)		// armo los subconjuntos O(n2^n)
+	for(int i = 0; i < (1<<n); i++)			// armo los subconjuntos O(n2^n)
 	{
 		wi = 0;
 		pi = 0;
@@ -43,8 +43,8 @@ int main() {
 		{
 			if(i & (1<<j))
 			{
-				wi += pedidos[j].first;
-				pi += pedidos[j].second;
+				wi += pedidos[j].first;		//sumo los pesos
+				pi += pedidos[j].second;	//sumo los beneficios
 			}
 		}
 		if (wi>w)
