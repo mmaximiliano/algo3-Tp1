abc69b9ab17fcaca0c135565db498f3882782dfb
mmaximiliano <maxii.martino@gmail.com>
2019-04-10 11:00:06 -0300

    Update fb_exp_plotter.py

--- exp/fb_exp_plotter.py
+++ exp/fb_exp_plotter.py
@@ -3,14 +3,12 @@ import pandas as pd
 import seaborn as sns
 sns.set()
 
-df = pd.read_csv("stairs_time.csv")
+df = pd.read_csv("times_fuerzaBruta.csv")
 
 # Nano -> Milli
-df['TopDown']   /= 1000000.0
-df['BottomUp']  /= 1000000.0
-df['BottomUp2'] /= 1000000.0
+df['time']   /= 1000000.0
 
-ax = df.plot('n', ['TopDown', 'BottomUp', 'BottomUp2'], marker='o', title='Comparación de tiempos - Dinámica')
+ax = df.plot('n', ['times'], marker='o', title='Comparación de tiempos - Fuerza Bruta')
 ax.set_xlabel('N')
 ax.set_ylabel('Tiempos (ms)')
 
