2f2d3e650cb187168943bed1f177a9fc43299ce5
mmaximiliano <31483803+mmaximiliano@users.noreply.github.com>
2019-04-12 16:19:00 -0300

    intento de superponer graficos

--- exp/fb/fb_exp_plotter.py
+++ exp/fb/fb_exp_plotter.py
@@ -1,8 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import numpy as np
 sns.set()
 
+#grafico complejidad real
+x = np.arange(30)
+y = x*np.power(2,x)
+plt.plot(x,y)
+
+
+#grafico complejidad de exp
 df = pd.read_csv("times_fuerzaBruta_n30.csv")
 
 # Nano -> Milli
@@ -11,5 +19,7 @@ df['time']  /= 1000000.0
 ax = df.plot('n', ['time'], marker='o', title='Comparación de tiempos - Dinámica')
 ax.set_xlabel('N')
 ax.set_ylabel('Tiempos (ms)')
+
+
 
 plt.show()
