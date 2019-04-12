50f0b09810b91a8a042dddb3fa0c84fb161735e0
Nico P <nicopironio@gmail.com>
2019-04-12 18:16:36 -0300

    python de middle hace 2 graficos

--- exp/middle/exp_middle_plotter.py
+++ exp/middle/exp_middle_plotter.py
@@ -1,15 +1,29 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import numpy as np
 sns.set()
+
+def teorica(x):
+	return 0.001*x*np.float_power(2,(x/2))
 
 df = pd.read_csv("times_middle.csv")
 
 # Nano -> Milli
 df['time']  /= 1000000.0
+df['teorica'] = 0.001*df['n'] * 2**(df['n'] / 2.0) 
 
-ax = df.plot('n', ['time'], marker='o', title='Comparación de tiempos - Meet In The Middle')
-ax.set_xlabel('N')
-ax.set_ylabel('Tiempos (ms)')
+
+df.plot('n', ['time', 'teorica'])
+
+ax1 = sns.lmplot(x='time', y='teorica', data=df);
+plt.xlabel("T(n)");
+plt.ylabel("cota(n)");
+
+#ax = df.plot('n', ['time'], marker='o', title='Comparación de tiempos - Meet In The Middle')
+# plt.plot('n', teorica(['n']), color='coral')
+#ax.set_xlabel('N')
+#ax.set_ylabel('Tiempos (ms)')
 
 plt.show()
+
