1ed0ec0b21690725d555312aaa3d78d4df757416
Marcos <mjblufstein@dc.uba.ar>
2019-04-13 14:06:29 -0300

    exp y resultados dp para heatmap

---
+++ exp/dp/exp_dp_heat.cpp
@@ -0,0 +1,93 @@
+#pragma GCC optimize("O3")
+#include <chrono>
+#include <random>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+
+int main()
+{
+  ll REPS = 30;
+
+  // Header del csv
+  cout << "-1,";
+  for(int n = 0; n <= 1000; n = n+50)
+  {
+    cout<<n<<",";
+  }
+  cout<<'\n';
+
+    //fijo w
+  for (int w = 0; w <= 1000; w = w+50)
+  {
+    cout<<w<<",";
+    //vario n
+    for(int n = 0; n <= 1000; n = n+50) {
+
+      ll total_fb = 0;
+      //creo vector de n posiciones
+      vector<pii> pedidos(n);
+
+      int wi, pi;
+      int res = 0;
+
+      //utilizo una distribucion uniforme
+      std::default_random_engine generator;
+      std::uniform_int_distribution<ll> distribution(1,w);
+
+      //relleno el vector con valores random
+      for(ll i = 0; i < n; i++)
+      {
+        wi = distribution(generator);
+        pi = distribution(generator);
+
+        pedidos[i] = make_pair(wi, pi);
+      }
+
+
+      for(int rep = 0; rep < REPS; ++rep) {
+
+        auto time_start = std::chrono::steady_clock::now();
+
+        int dp[n+1][w+1];
+
+        for(int i = 0; i <= n; i++){
+          for(int j = 0; j <= w; j++){
+            if(i==0 || j==0){
+              dp[i][j] = 0;
+            }else{
+              if(pedidos[i-1].first <= j){
+                dp[i][j] = max(dp[i-1][j], pedidos[i-1].second + dp[i-1][j-pedidos[i-1].first]);
+              }else{
+                dp[i][j] = dp[i-1][j];
+              }
+            }
+          }
+        }
+        res = dp[n][w];
+
+        auto time_end = std::chrono::steady_clock::now();
+        ll time_fb = std::chrono::duration_cast<std::chrono::nanoseconds>(time_end - time_start).count();
+
+        total_fb += time_fb;
+      }
+
+      // Promedio de tiempo
+      total_fb /= REPS;
+
+      // Escribo a stdout, podria escribir directamente en un archivo especifico
+      cout << total_fb  << ",";
+    }
+    cout<<'\n';
+  }
+
+  return 0;
+}
