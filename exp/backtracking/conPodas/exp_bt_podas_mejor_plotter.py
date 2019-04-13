e82b3091b60ab7e49f04614ecae411d667d68fe7
Marcos <mjblufstein@dc.uba.ar>
2019-04-13 12:47:30 -0300

    graficos podas mejor caso

---
+++ exp/backtracking/conPodas/exp_bt_podas_mejor_plotter.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+sns.set()
+
+df = pd.read_csv("times_bt_podas_mejor_caso.csv")
+
+# Nano -> Milli
+df['time']  /= 1000000.0
+df['teorica'] = 0.001*df['n'] * 2**(df['n'])
+
+df.plot('n', ['time', 'teorica'])
+
+ax1 = sns.lmplot(x='time', y='teorica', data=df);
+plt.xlabel("T(n)");
+plt.ylabel("cota(n)");
+
+plt.show()
