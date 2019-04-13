8e54ba5409f77f1519b225d14c6a03c02557256c
Marcos <mjblufstein@dc.uba.ar>
2019-04-13 12:41:08 -0300

    graficos fb

--- exp/fb/fb_exp_plotter.py
+++ exp/fb/fb_exp_plotter.py
@@ -15,11 +15,12 @@ df = pd.read_csv("times_fuerzaBruta_n30.csv")
 
 # Nano -> Milli
 df['time']  /= 1000000.0
+df['teorica'] = 0.001*df['n'] * 2**(df['n'])
 
-ax = df.plot('n', ['time'], marker='o', title='Comparación de tiempos - Dinámica')
-ax.set_xlabel('N')
-ax.set_ylabel('Tiempos (ms)')
+df.plot('n', ['time', 'teorica'])
 
-
+ax1 = sns.lmplot(x='time', y='teorica', data=df);
+plt.xlabel("T(n)");
+plt.ylabel("cota(n)");
 
 plt.show()
